129c4acc6390309fbb6b81ecc53b3e6083674f39
KreKer_ <platon.bob@gmail.com>
2024-11-17 14:19:05 +0300

    Game scene updated + switchScene script updated

--- AllColors/AllColors/Assets/Scripts/ScreenFillingSquares.cs
+++ AllColors/AllColors/Assets/Scripts/ScreenFillingSquares.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.Mime;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -13,33 +14,27 @@ public class ScreenFillingSquares : MonoBehaviour
     public Color[] colors;           // Массив цветов
     public ColorPicker2D _colorPicker2D;
 
-    
     [SerializeField] private TMP_Text colorRule;
     [SerializeField] private TMP_Text score;
-    
+    [SerializeField] private Slider timeSlider;
+    [SerializeField] private GameObject overScreen;
+
     private float waitTime = 2f;
     private float regenerateTime = 2f;
     private List<GameObject> squares = new List<GameObject>();
     private Color currentColor;
-    
 
     void Start()
     {
         StartCoroutine(CycleSquares());
     }
 
-
-
     void GenerateSquares()
     {
-        // Получаем размеры экрана в мировых координатах
         float screenWidth = 2 * Camera.main.orthographicSize * Camera.main.aspect;
         float screenHeight = 2 * Camera.main.orthographicSize;
+        float squareSize = Mathf.Min(screenWidth, screenHeight) / 10;
 
-        // Размер квадрата (высота и ширина должны быть одинаковыми)
-        float squareSize = Mathf.Min(screenWidth, screenHeight) / 10;  // Количество квадратов в ряд/столбец
-
-        // Определяем количество квадратов по ширине и высоте
         int squaresX = Mathf.CeilToInt(screenWidth / squareSize);
         int squaresY = Mathf.CeilToInt(screenHeight / squareSize);
 
@@ -47,15 +42,11 @@ public class ScreenFillingSquares : MonoBehaviour
         {
             for (int y = 1; y < squaresY - 1; y++)
             {
-                // Позиция для квадрата
                 Vector2 position = new Vector2(-screenWidth / 2 + x * squareSize + squareSize / 2, -screenHeight / 2 + y * squareSize + squareSize / 2);
-
-                // Создание квадрата
                 GameObject newSquare = Instantiate(squarePrefab, position, Quaternion.identity);
-                newSquare.transform.localScale = new Vector3(squareSize, squareSize, 1);  // Установка размера квадрата
+                newSquare.transform.localScale = new Vector3(squareSize, squareSize, 1);
                 squares.Add(newSquare);
 
-                // Назначение случайного цвета из массива
                 SpriteRenderer spriteRenderer = newSquare.GetComponent<SpriteRenderer>();
                 if (spriteRenderer != null && colors.Length > 0)
                 {
@@ -64,9 +55,17 @@ public class ScreenFillingSquares : MonoBehaviour
             }
         }
     }
-    IEnumerator KeepOneColor( )
+
+    IEnumerator KeepOneColor()
     {
-        yield return new WaitForSeconds(waitTime);
+        float elapsedTime = 0f;
+        while (elapsedTime < waitTime)
+        {
+            elapsedTime += Time.deltaTime;
+            timeSlider.value = elapsedTime / waitTime;
+            yield return null;
+        }
+
         foreach (GameObject square in squares)
         {
             if (currentColor != square.GetComponent<SpriteRenderer>().color)
@@ -79,23 +78,51 @@ public class ScreenFillingSquares : MonoBehaviour
         if (currentColor == _colorPicker2D.pointedColor)
         {
             score.text = (int.Parse(score.text) + 1).ToString();
+            if (waitTime > 0.1f)
+                waitTime -= 0.1f;
+            else
+                waitTime = 0.1f;
+        }
+        else
+        {
+            GameOver();
         }
     }
+
+    private void GameOver()
+    {
+        Time.timeScale = 0f;
+        overScreen.SetActive(true);
+    }
+
     IEnumerator CycleSquares()
     {
         while (true)
         {
             GenerateSquares();
 
+            int currentScore = int.Parse(score.text);
             currentColor = colors[Random.Range(0, colors.Length)];
-            colorRule.color = currentColor;
+
+            if (currentScore < 5)
+            {
+                colorRule.color = currentColor;
+                colorRule.text = ColorToText.ColorToName(currentColor);
+            }
+            else
+            {
+                var randomColorName = ColorToText.GetRandomColorName();
+                colorRule.color = currentColor;
+                colorRule.text = randomColorName;
+            }
+
             yield return StartCoroutine(KeepOneColor());
 
             yield return new WaitForSeconds(regenerateTime);
 
             foreach (GameObject square in squares)
             {
-                if(square != null)
+                if (square != null)
                     Destroy(square);
             }
             squares.Clear();
