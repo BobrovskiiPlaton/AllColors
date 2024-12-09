a5249055fa9ec9d916e5351be644e39c2731fd84
KreKer_ <platon.bob@gmail.com>
2024-12-09 17:28:00 +0300

    stuff

--- AllColors/AllColors/Assets/Scripts/ScreenFillingSquares.cs
+++ AllColors/AllColors/Assets/Scripts/ScreenFillingSquares.cs
@@ -21,7 +21,6 @@ public class ScreenFillingSquares : MonoBehaviour
     [SerializeField] private GameObject overScreen;
 
     private float waitTime = 3f;
-    private float regenerateTime = 2f;
     private List<GameObject> squares = new List<GameObject>();
     private Color currentColor;
     private bool squaresFloating = false;
@@ -30,12 +29,74 @@ public class ScreenFillingSquares : MonoBehaviour
     {
         StartCoroutine(CycleSquares());
     }
+    
+    
 
-    void Update()
+    IEnumerator CycleSquares()
     {
+        while (true)
+        {
+            GenerateField();
+            UpdateRule();
+
+            float elapsedTime = 0f;
+            while (elapsedTime < waitTime)
+            {
+                elapsedTime += Time.deltaTime;
+                timeSlider.value = elapsedTime / waitTime;
+                yield return null;
+            }
+            
+            if (currentColor != _colorPicker2D.pointedColor)
+            {
+                GameOver();
+                yield break;
+                
+            }
+            AddPoint();
+            DecreaseTimer();
+            DeleteField();
+        }
     }
 
-    void GenerateSquares()
+    private void AddPoint()
+    {
+        score.text = (int.Parse(score.text) + 1).ToString();
+        scoreAudio.Play(); // Воспроизводим звук при наборе очков
+    }
+
+    private void UpdateRule()
+    {
+        int currentScore = int.Parse(score.text);
+        currentColor = colors[Random.Range(0, colors.Length)];
+        if (currentScore >= 10)
+        {
+            squaresFloating = true;
+        }
+
+        if (currentScore >= 5)
+        {
+            var randomColorName = ColorToText.GetRandomColorName();
+            colorRule.color = currentColor;
+            colorRule.text = randomColorName;
+        }
+        else
+        {
+            colorRule.color = currentColor;
+            colorRule.text = ColorToText.ColorToName(currentColor);
+        }
+    }
+
+    private void DecreaseTimer()
+    {
+        if (waitTime > 1f)
+            waitTime -= 0.1f;
+        else
+            waitTime = 1f;
+    }
+
+    
+    void GenerateField()
     {
         float screenWidth = 2 * Camera.main.orthographicSize * Camera.main.aspect;
         float screenHeight = 2 * Camera.main.orthographicSize;
@@ -52,48 +113,39 @@ public class ScreenFillingSquares : MonoBehaviour
                 GameObject newSquare = Instantiate(squarePrefab, position, Quaternion.identity);
                 newSquare.transform.localScale = new Vector3(squareSize, squareSize, 1);
                 squares.Add(newSquare);
-
-                SpriteRenderer spriteRenderer = newSquare.GetComponent<SpriteRenderer>();
-                if (spriteRenderer != null && colors.Length > 0)
+                
+                if (newSquare.GetComponent<SpriteRenderer>() != null && colors.Length > 0)
                 {
-                    spriteRenderer.color = colors[Random.Range(0, colors.Length)];
+                    newSquare.GetComponent<SpriteRenderer>().color = colors[Random.Range(0, colors.Length)];
                 }
 
                 if (squaresFloating)
                 {
-                    Rigidbody2D rb = newSquare.AddComponent<Rigidbody2D>();
-                    rb.gravityScale = 0f;  // Убираем влияние гравитации
-                    rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous; // Улучшенная детекция столкновений
-                    rb.constraints = RigidbodyConstraints2D.None; // Убираем все ограничения
-                    BoxCollider2D collider = newSquare.GetComponent<BoxCollider2D>();
-                    if (collider == null)
-                    {
-                        collider = newSquare.AddComponent<BoxCollider2D>();
-                    }
-                    collider.isTrigger = false; // Квадраты могут пересекаться и сталкиваться
-                    Vector2 randomDirection = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
-                    rb.velocity = randomDirection;
+                    Vector2 randomDirection = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+                    newSquare.GetComponent<Rigidbody2D>().velocity = randomDirection;
                 }
             }
         }
     }
-
-    IEnumerator KeepOneColor()
+    private void DeleteField()
     {
-        ActivateColliders();
-
-        float elapsedTime = 0f;
-        while (elapsedTime < waitTime)
+        foreach (GameObject square in squares)
         {
-            if(_colorPicker2D.pointedColor == Color.black)
-                GameOver();
-            elapsedTime += Time.deltaTime;
-            timeSlider.value = elapsedTime / waitTime;
-            yield return null;
+            if (square != null)
+                Destroy(square);
         }
+        squares.Clear();
+    }
 
-        DeactivateColliders();
+    private void GameOver()
+    {
+        ShowCorrectColor();
+        Time.timeScale = 0f;
+        overScreen.SetActive(true);
+    }
 
+    private void ShowCorrectColor()
+    {
         foreach (GameObject square in squares)
         {
             if (currentColor != square.GetComponent<SpriteRenderer>().color)
@@ -102,90 +154,5 @@ public class ScreenFillingSquares : MonoBehaviour
             }
         }
         squares.RemoveAll(s => s == null || s.GetComponent<SpriteRenderer>().color != currentColor);
-
-    }
-
-    private void GameOver()
-    {
-        Time.timeScale = 0f;
-        overScreen.SetActive(true);
-    }
-
-    private void ActivateColliders()
-    {
-        foreach (var square in squares)
-        {
-            var collider = square.GetComponent<BoxCollider2D>();
-            if (collider == null)
-            {
-                collider = square.AddComponent<BoxCollider2D>();
-            }
-            collider.isTrigger = true; // Активируем триггер
-        }
-    }
-
-    private void DeactivateColliders()
-    {
-        foreach (var square in squares)
-        {
-            var collider = square.GetComponent<BoxCollider2D>();
-            if (collider != null)
-            {
-                Destroy(collider); // Удаляем коллайдер
-            }
-        }
-    }
-
-    IEnumerator CycleSquares()
-    {
-        while (true)
-        {
-            int currentScore = int.Parse(score.text);
-            if (currentScore >= 10)
-            {
-                squaresFloating = true;
-            }
-
-            GenerateSquares();
-
-            currentColor = colors[Random.Range(0, colors.Length)];
-
-            if (currentScore < 5)
-            {
-                colorRule.color = currentColor;
-                colorRule.text = ColorToText.ColorToName(currentColor);
-            }
-            else
-            {
-                var randomColorName = ColorToText.GetRandomColorName();
-                colorRule.color = currentColor;
-                colorRule.text = randomColorName;
-            }
-
-            yield return StartCoroutine(KeepOneColor());
-            if (currentColor == _colorPicker2D.pointedColor)
-            {
-                score.text = (int.Parse(score.text) + 1).ToString();
-                scoreAudio.Play(); // Воспроизводим звук при наборе очков
-
-                if (waitTime > 1f)
-                    waitTime -= 0.1f;
-                else
-                    waitTime = 0.1f;
-            }
-            else
-            {
-                GameOver();
-                yield break;
-            }
-            //yield return new WaitForSeconds(regenerateTime);
-
-            foreach (GameObject square in squares)
-            {
-                if (square != null)
-                    Destroy(square);
-            }
-            squares.Clear();
-        }
     }
 }
